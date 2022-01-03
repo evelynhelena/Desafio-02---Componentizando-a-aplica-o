6cc1bb91d2f1f596ffc9ff7d8ee24b61ea439f77
evelynhelena <evelynhelena781@gmail.com>
2022-01-03 19:54:19 -0300

    terminando desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from '../components/Button';
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+interface SideBarProps {
+  changeGenre: (id: number) => void;
+  idGenre: number;
+}
+
+export function SideBar({changeGenre , idGenre}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => changeGenre(genre.id)}
+          selected={idGenre === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
+}          
